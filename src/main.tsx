03ee8008a70eb59a98cd990b905c78fe182e73a2
Cameron <cam.mclagan@gmail.com>
2025-09-20 19:38:38 -0700

    status setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <p>Init :D</p>
+    <p>Init :D (with status checking setup!)</p>
   </StrictMode>
 )
